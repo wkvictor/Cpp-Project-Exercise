fc5e72345bd3655e83d42ac4c46af89531713473
Victor <victor.wangkun@gmail.com>
2017-04-07 13:01:04 -0700

    Finish TextInput Class

---
+++ master_mind/TextInput.cpp
@@ -0,0 +1,84 @@
+#include "TextInput.h"
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+TextInput::TextInput(const vector<string>& colorNames) :
+    _lengthOfPattern(0), _numColorsInUse(0), _palette("") {
+    for(vector<string>::const_iterator iter = colorNames.begin(); iter != colorNames.end(); ++iter)
+    	_palette.push_back((*iter)[0]);
+}
+
+int TextInput::_readInt(const string& prompt, int small, int large) const {
+	string buffer;
+	int answer = small - 1;     // intentionally invalid
+	while (answer < small || answer > large) {
+		cout << prompt << " (from " << small << " to " << large << ")? ";
+		cin >> buffer;
+		stringstream converter;
+		converter << buffer;
+		converter >> answer;
+		if (!converter.fail()) {
+			if (answer < small || answer > large) 
+				cout << "Integer must be from " << small << " to " << large << "." << endl;
+		}
+		else {
+			cout << "That is not a valid integer." << endl;
+		}
+	}
+	return answer;
+}
+
+int TextInput::queryLengthOfPattern() {
+	_lengthOfPattern = _readInt("How many pegs are in the secret", 1, 10);
+	return _lengthOfPattern;
+}
+
+int TextInput::queryNumberOfColors() {
+	_numColorsInUse = _readInt("How many colors are available", 2, _palette.size());
+	return _numColorsInUse;
+}
+
+int TextInput::queryNumberOfTurns() const {
+	return _readInt("How many turns are allowed", 1, 20);
+}
+
+bool TextInput::queryNewGame() const {
+	cout << endl;
+	cout << "Would you like to play again? " << endl;
+	string answer;
+	cin >> answer;
+	return (answer == "y" || answer == "Y" || answer == "yes" 
+		    || answer == "YES" || answer == "Yes");
+}
+
+Pattern TextInput::enterGuess() const {
+	Pattern pattern(_lengthOfPattern);
+	string currentPalette = _palette.substr(0, _numColorsInUse);
+	string patternString;
+	bool validPattern = false;
+	while (!validPattern) {
+		cout << endl;
+		cout << "Enter a guess (colors are ";
+		cout << _palette,substr(0,_numColorsInUse) << "): ";
+		cin >> patternString;
+
+		validPattern = true;
+		if(patternString.size() != _lengthOfPattern) {
+			cout << "The pattern must have " << _lengthOfPattern << " pegs" << endl;
+			validPattern = false;
+		} else {
+			for(int i = 0; i < _lengthOfPattern; ++i) 
+				if (currentPalette.find(toupper(patternString[i])) > _lengthOfPattern)
+					validPattern = false;
+			if(!validPattern) 
+				cout << "The color options are " << currentPalette << endl;
+		}
+
+		if(validPattern) 
+			for(int i = 0; i < _lengthOfPattern; ++i) 
+				pattern.setPegColor(i, _palette.find(toupper(patternString[i])));
+	}
+	return pattern;
+}
+
---
+++ master_mind/TextInput.h
@@ -0,0 +1,73 @@
+#ifndef TEXTINPUT_H
+#define TEXTINPUT_H
+
+#include "Pattern.h"
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+/**
+ * \brief Class for dealing with text-based input for the Mastermind game
+ */
+class TextInput {
+private:
+	int _lengthOfPattern;
+	int _numColorsInUse;
+	string _palette;
+
+public:
+	/**
+	 * \brief Create a new text input instance.
+	 *
+	 * \param colorNames a list of strings (each color must start with a different letter)
+	 */
+	TextInput(const vector<string>& colorNames);
+
+	/**
+	 * \brief Ask the user how many pegs in the secret pattern
+	 * 
+	 * The length of the pattern is also stored internally
+	 *
+	 * \return the length of the pattern
+	 */
+	int queryLengthOfPattern();
+
+	/**
+	 * \brief Ask the user how many colors to use for secret pattern
+	 *
+	 * The number of colors is also stored internally.
+	 *
+	 * \return the number of colors
+	 */
+	int queryNumberofColors();
+
+	/**
+	 * \brief Ask the user maximum number of guesses to be allowed
+	 *
+	 * \return the maximum number of guesses
+	 */
+	int queryNumberofTurns() const;
+
+	/**
+	 * \brief Offer the user a new game.
+	 * 
+	 * \return true if accepted, false otherwise
+	 */
+	bool queryNewGame() const;
+
+	/**
+	 * \brief Get a guess from the user and return it as a Pattern instance
+	 *
+	 * \return the pattern entered
+	 */
+	Pattern enterGuess() const;
+
+private:
+	/**
+	 * \brief Robustly prompt the user for an integer from small to large
+	 */
+	int _readInt(const string& prompt, int small, int large) const;
+};
+
+#endif
